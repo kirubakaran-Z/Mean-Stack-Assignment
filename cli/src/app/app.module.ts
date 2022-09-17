f3a58d72022ff5d7cea42438abd74c272efb4410
kirubakaran-Z <kirubadurai0@gmail.com>
2022-09-17 23:02:48 +0530

    Created LogIn route and Log in fourm component

--- cli/src/app/app.module.ts
+++ cli/src/app/app.module.ts
@@ -4,8 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent, LoginComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
